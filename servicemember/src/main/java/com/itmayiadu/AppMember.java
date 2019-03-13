2a7548ee11235e3f12eda9344de3bfd8d8f54afb
admin <wulinemail163@163.com>
2019-03-13 23:17:11 +0800

    吴林

---
+++ servicemember/src/main/java/com/itmayiadu/AppMember.java
@@ -0,0 +1,16 @@
+package com.itmayiadu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AppMember {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMember.class, args);
+	}
+
+}
