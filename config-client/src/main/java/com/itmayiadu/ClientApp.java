c53c75287a571e85790c921001cdf5921de44443
admin <wulinemail163@163.com>
2019-05-01 17:16:05 +0800

    工程搭建

---
+++ config-client/src/main/java/com/itmayiadu/ClientApp.java
@@ -0,0 +1,14 @@
+package com.itmayiadu;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ClientApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApp.class, args);
+    }
+
+
+}
--- config-client/src/main/java/com/itmayiadu/controller/TestController.java
+++ config-client/src/main/java/com/itmayiadu/controller/TestController.java
@@ -1,6 +1,16 @@
 package com.itmayiadu.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+    @Value("${userName}")
+    String userName;
 
-
+    @RequestMapping(value = "/getUserName")
+    public String getUserName () {
+        return userName;
+    }
 }
